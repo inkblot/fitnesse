cf9e5efc3ff9f0a7938f0aa8b09146cd78ea5e00
Nate Riffe <inkblot@movealong.org>
2011-08-18 01:55:28 -0500

    Move some more production/test code to the appropriate source path

--- src/fitnesse/FitNesseVersion.java
+++ src/fitnesse/FitNesseVersion.java
@@ -6,7 +6,7 @@ public class FitNesseVersion {
   private final String version;
 	
   public FitNesseVersion() {
-    this("v20110713");
+    this("v20110818");
   }
 
   public FitNesseVersion(String version) {

9e1371443d8209933ecf5044577463c9e966461b
Nate Riffe <inkblot@movealong.org>
2011-08-18 13:27:30 -0500

    Stop generating code from templates entirely and write the version to a properties file, clean up the filesets that go into the jar, and introduce the 'resources' directory as a place for non-generated jar contents (e.g. tasks.properties).

--- src/fitnesse/FitNesseVersion.java
+++ src/fitnesse/FitNesseVersion.java
@@ -2,24 +2,51 @@
 // Released under the terms of the GNU General Public License version 2 or later.
 package fitnesse;
 
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Properties;
+
 public class FitNesseVersion {
-  private final String version;
-	
-  public FitNesseVersion() {
-    this("v20110818");
-  }
+    private final String version;
 
-  public FitNesseVersion(String version) {
-    this.version = version;
-  }
+    private static Properties getBuildProperties() {
+        Properties buildProperties = new Properties();
+        InputStream propertyStream = null;
+        Reader propertyReader = null;
+        try {
+            propertyStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("fitnesse/build.properties");
+            propertyReader = new InputStreamReader(propertyStream);
+            buildProperties.load(propertyReader);
+        } catch (NullPointerException e) {
+            throw new RuntimeException("Could not load build.properties from the classpath.  Are you sure the jar is packaged correctly?", e);
+        } catch (IOException e) {
+            throw new RuntimeException("Could not load build.properties from the classpath.  Are you sure the jar is packaged correctly?", e);
+        } finally {
+            IOUtils.closeQuietly(propertyReader);
+            IOUtils.closeQuietly(propertyStream);
+        }
+        return buildProperties;
+    }
 
-  public String toString() {
-    return version;
-  }
+    public FitNesseVersion() {
+        this(getBuildProperties().getProperty("build.version"));
+    }
 
-  public boolean isAtLeast(String requiredVersion) {
-    long thisVersion = Long.parseLong(version.substring(1));
-    long otherVersion = Long.parseLong(requiredVersion.substring(1));
-    return thisVersion >= otherVersion;
-  }
+    public FitNesseVersion(String version) {
+        this.version = version;
+    }
+
+    public String toString() {
+        return version;
+    }
+
+    public boolean isAtLeast(String requiredVersion) {
+        long thisVersion = Long.parseLong(version.substring(1));
+        long otherVersion = Long.parseLong(requiredVersion.substring(1));
+        return thisVersion >= otherVersion;
+    }
 }

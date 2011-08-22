5cd424950b3c11361801457ca6c0a594f0e3c703
Nate Riffe <inkblot@movealong.org>
2011-08-21 23:53:36 -0500

    Get rid of some unused stuff in WIkiPageDummy

--- src/fitnesse/wiki/WikiPageDummy.java
+++ src/fitnesse/wiki/WikiPageDummy.java
@@ -4,118 +4,115 @@
 package fitnesse.wiki;
 
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import util.Clock;
 
 public class WikiPageDummy implements WikiPage {
-  private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-  public String name;
-  protected String location;
-  private PageData pageData;
-  private WikiPage parent;
-  protected WikiPage parentForVariables;
+    public String name;
+    protected String location;
+    private PageData pageData;
+    private WikiPage parent;
+    protected WikiPage parentForVariables;
 
-  public static final int daysTillVersionsExpire = 14;
+    public WikiPageDummy(String name, String content) throws Exception {
+        this.name = name;
+        pageData = new PageData(this, content);
+    }
 
-  public WikiPageDummy(String name, String content) throws Exception {
-    this.name = name;
-    pageData = new PageData(this, content);
-  }
+    public WikiPageDummy(String location) {
+        this.location = location;
+        name = "Default";
+    }
 
-  public WikiPageDummy(String location) {
-    this.location = location;
-    name = "Default";
-  }
+    public WikiPageDummy() {
+        location = null;
+        name = "Default";
+    }
 
-  public WikiPageDummy() {
-    location = null;
-    name = "Default";
-  }
+    public String getName() {
+        return name;
+    }
 
-  public String getName() {
-    return name;
-  }
+    public WikiPage getParent() {
+        return parent;
+    }
 
-  public WikiPage getParent() {
-    return parent;
-  }
+    public void setParentForVariables(WikiPage parent) {
+        parentForVariables = parent;
+    }
 
-  public void setParentForVariables(WikiPage parent) {
-    parentForVariables = parent;
-  }
+    public WikiPage getParentForVariables() throws Exception {
+        return parentForVariables == null ? this : parentForVariables;
+    }
 
-  public WikiPage getParentForVariables() throws Exception {
-    return parentForVariables == null ? this : parentForVariables;
-  }
+    public void setParent(WikiPage parent) {
+        this.parent = this.parentForVariables = parent;
+    }
 
-  public void setParent(WikiPage parent) {
-    this.parent = this.parentForVariables = parent;
-  }
+    public PageData getData() throws Exception {
+        return pageData;
+    }
 
-  public PageData getData() throws Exception {
-    return pageData;
-  }
+    public VersionInfo commit(PageData data) throws Exception {
+        pageData = data;
+        return new VersionInfo("mockVersionName", "mockAuthor", Clock.currentDate());
+    }
 
-  public VersionInfo commit(PageData data) throws Exception {
-    pageData = data;
-    return new VersionInfo("mockVersionName", "mockAuthor", Clock.currentDate());
-  }
+    public List<WikiPage> getChildren() {
+        return new ArrayList<WikiPage>();
+    }
 
-  public List<WikiPage> getChildren() {
-    return new ArrayList<WikiPage>();
-  }
+    public int compareTo(Object o) {
+        return 0;
+    }
 
-  public int compareTo(Object o) {
-    return 0;
-  }
+    public PageData getDataVersion(String versionName) throws Exception {
+        return null;
+    }
 
-  public PageData getDataVersion(String versionName) throws Exception {
-    return null;
-  }
+    public void removeChildPage(String name) throws Exception {
+    }
 
-  public void removeChildPage(String name) throws Exception {
-  }
+    public PageCrawler getPageCrawler() {
+        return new PageCrawlerImpl();
+    }
 
-  public PageCrawler getPageCrawler() {
-    return new PageCrawlerImpl();
-  }
+    public WikiPage getHeaderPage() throws Exception {
+        return null;
+    }
 
-  public WikiPage getHeaderPage() throws Exception {
-    return null;
-  }
+    public WikiPage getFooterPage() throws Exception {
+        return null;
+    }
 
-  public WikiPage getFooterPage() throws Exception {
-    return null;
-  }
+    public WikiPage addChildPage(String name) throws Exception {
+        return null;
+    }
 
-  public WikiPage addChildPage(String name) throws Exception {
-    return null;
-  }
+    public boolean hasChildPage(String name) throws Exception {
+        return false;
+    }
 
-  public boolean hasChildPage(String name) throws Exception {
-    return false;
-  }
+    public WikiPage getChildPage(String name) throws Exception {
+        return null;
+    }
 
-  public WikiPage getChildPage(String name) throws Exception {
-    return null;
-  }
+    public boolean hasExtension(String extensionName) {
+        return false;
+    }
 
-  public boolean hasExtension(String extensionName) {
-    return false;
-  }
+    public Extension getExtension(String extensionName) {
+        return null;
+    }
 
-  public Extension getExtension(String extensionName) {
-    return null;
-  }
+    public String getHelpText() throws Exception {
+        return "Dummy help text";
+    }
 
-  public String getHelpText() throws Exception {
-    return "Dummy help text";
-  }
-
-  public List<WikiPageAction> getActions() throws Exception {
-    return null;
-  }
+    public List<WikiPageAction> getActions() throws Exception {
+        return null;
+    }
 }

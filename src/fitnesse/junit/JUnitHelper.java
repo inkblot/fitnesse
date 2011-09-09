af38e7f5d0a37e7ba83e4bdf4ed1a660e8926263
Nate Riffe <inkblot@movealong.org>
2011-09-09 07:58:37 -0500

    Fold TestHelper into JUnitHelper, the class's only consumer.

--- src/fitnesse/junit/JUnitHelper.java
+++ src/fitnesse/junit/JUnitHelper.java
@@ -2,14 +2,25 @@ package fitnesse.junit;
 
 
 import com.google.inject.Injector;
+import fitnesse.responders.run.JavaFormatter;
 import fitnesse.responders.run.ResultsListener;
 import fitnesse.responders.run.TestSummary;
+import fitnesseMain.FitNesseMain;
 import junit.framework.Assert;
 
 public class JUnitHelper {
 
-    private final TestHelper helper;
+    private static final String PAGE_TYPE_SUITE = "suite";
+    private static final String PAGE_TYPE_TEST = "test";
+    private final static String COMMON_ARGS = "&nohistory=true&format=java";
+    private final static String DEBUG_ARG = "&debug=true";
+
     private int port = 0;
+    private boolean debug = true;
+    private String fitNesseDir;
+    private String outputDir;
+    private ResultsListener resultsListener;
+    private Injector injector;
 
     public void setPort(int port) {
         this.port = port;
@@ -21,23 +32,57 @@ public class JUnitHelper {
 
     public JUnitHelper(String fitNesseDir, String outputDir,
                        ResultsListener resultsListener, Injector injector) {
-        helper = new TestHelper(fitNesseDir, outputDir, resultsListener, injector);
+        this.fitNesseDir = fitNesseDir;
+        this.outputDir = outputDir;
+        this.resultsListener = resultsListener;
+        this.injector = injector;
+    }
+
+    public TestSummary run(String pageName, String pageType, String suiteFilter, int port) throws Exception {
+        JavaFormatter testFormatter = JavaFormatter.getInstance(pageName);
+        testFormatter.setResultsRepository(new JavaFormatter.FolderResultsRepository(outputDir, fitNesseDir));
+        testFormatter.setListener(resultsListener);
+        FitNesseMain.Arguments arguments = new FitNesseMain.Arguments();
+        arguments.setDaysTillVersionsExpire("0");
+        arguments.setInstallOnly(false);
+        arguments.setOmitUpdates(true);
+        arguments.setPort(String.valueOf(port));
+        arguments.setRootPath(fitNesseDir);
+        arguments.setCommand(getCommand(pageName, pageType, suiteFilter));
+        FitNesseMain.launchFitNesse(arguments, injector);
+        return testFormatter.getTotalSummary();
+    }
+
+    String getCommand(String pageName, String pageType, String suiteFilter) {
+        String commandPrefix = pageName + "?" + pageType;
+        if (suiteFilter != null)
+            return commandPrefix + getCommandArgs() + "&suiteFilter=" + suiteFilter;
+        else
+            return commandPrefix + getCommandArgs();
+    }
+
+    private String getCommandArgs() {
+        if (debug) {
+            return DEBUG_ARG + COMMON_ARGS;
+        }
+        return COMMON_ARGS;
     }
 
     public void setDebugMode(boolean enabled) {
-        helper.setDebugMode(enabled);
+        debug = enabled;
     }
 
+
     public void assertTestPasses(String testName) throws Exception {
-        assertPasses(testName, TestHelper.PAGE_TYPE_TEST, null);
+        assertPasses(testName, PAGE_TYPE_TEST, null);
     }
 
     public void assertSuitePasses(String suiteName, String suiteFilter) throws Exception {
-        assertPasses(suiteName, TestHelper.PAGE_TYPE_SUITE, suiteFilter);
+        assertPasses(suiteName, PAGE_TYPE_SUITE, suiteFilter);
     }
 
     public void assertPasses(String pageName, String pageType, String suiteFilter) throws Exception {
-        TestSummary summary = helper.run(pageName, pageType, suiteFilter, port);
+        TestSummary summary = run(pageName, pageType, suiteFilter, port);
         Assert.assertEquals("wrong", 0, summary.wrong);
         Assert.assertEquals("exceptions", 0, summary.exceptions);
         Assert.assertTrue("at least one test executed", summary.right > 0);
---
+++ test/fitnesse/junit/JUnitHelperTest.java
@@ -0,0 +1,43 @@
+package fitnesse.junit;
+
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import fitnesse.FitnesseBaseTestCase;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class JUnitHelperTest extends FitnesseBaseTestCase {
+
+    @Inject
+    public Injector injector;
+
+    private JUnitHelper helper;
+
+    @Before
+    public void setUp() {
+        helper = new JUnitHelper("", "", injector);
+    }
+
+    @After
+    public void tearDown() {
+        helper = null;
+    }
+
+    @Test
+    public void getCommand_formatting() {
+        assertEquals("test, no filter", "TestName?test&debug=true&nohistory=true&format=java", helper.getCommand("TestName", "test", null));
+        assertEquals("suite, no filter", "SuiteName?suite&debug=true&nohistory=true&format=java", helper.getCommand("SuiteName", "suite", null));
+        assertEquals("suite, with filter", "SuiteName?suite&debug=true&nohistory=true&format=java&suiteFilter=xxx", helper.getCommand("SuiteName", "suite", "xxx"));
+    }
+
+    @Test
+    public void getCommand_formatting_without_debug() {
+        helper.setDebugMode(false);
+        assertEquals("test, no filter", "TestName?test&nohistory=true&format=java", helper.getCommand("TestName", "test", null));
+        assertEquals("suite, no filter", "SuiteName?suite&nohistory=true&format=java", helper.getCommand("SuiteName", "suite", null));
+        assertEquals("suite, with filter", "SuiteName?suite&nohistory=true&format=java&suiteFilter=xxx", helper.getCommand("SuiteName", "suite", "xxx"));
+    }
+}
